4a249ce1be43ca7a995fd96558c8b3625bb78816
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-07-20 00:51:39 +0300

    do task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,30 @@ import Contacts from 'components/Contacts';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+  componentDidMount() {
+    console.log('App componentDidMount');
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App componentDidUpdate');
+
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Обновились контакты');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = ({ name, number }) => {
     const { contacts } = this.state;
@@ -28,15 +45,6 @@ export class App extends Component {
     const checkedContact = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
-
-    // if (checkedContact) {
-    //   alert(`${name} is already in contacts`);
-    //   return;
-    // }
-
-    // this.setState(prevState => ({
-    //   contacts: [contact, ...prevState.contacts],
-    // }));
 
     checkedContact
       ? alert(`${name} is already in contacts`)
